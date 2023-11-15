1f5247b5825991e188020b10fb2931ae97e58ae7
Danielle Maia <daniellemaia01@gmail.com>
2023-11-15 01:28:39 -0300

    Criação do projeto.

---
+++ atividade_final/src/main/java/br/unifor/ads/software/atividade_final/MainAtividadeFinal.java
@@ -0,0 +1,8 @@
+package br.unifor.ads.software.atividade_final;
+
+public class MainAtividadeFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
