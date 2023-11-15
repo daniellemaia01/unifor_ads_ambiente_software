89f27d036b3fc79327c2a384e4985f6c5f9f1fb3
Danielle Maia <daniellemaia01@gmail.com>
2023-11-15 01:57:30 -0300

    Criando projeto na IDE Eclipse.

---
+++ atividade_final/src/main/java/br/unifor/ads/software/atividade_final/Grupo.java
@@ -0,0 +1,18 @@
+
+package br.unifor.ads.software.atividade_final;
+
+public class Grupo {
+    
+    //Atributos
+    
+    public String nome;
+    public int idade;
+    public String curso;
+    
+    // Métodos
+    
+    public void apresentar() {
+        
+    }
+    
+}
