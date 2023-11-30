6ac8f58ba34a28c9868a1c59cb141e5cd7a51f34
João Neto <jnettto@gmail.com>
2023-11-29 21:04:26 -0300

    Pneu.java

---
+++ atividade_final/src/main/java/br/unifor/ads/software/atividade_final/Pneu.java
@@ -0,0 +1,20 @@
+// Classe João Neto
+
+package br.unifor.ads.software.atividade_final;
+
+public class Pneu {
+
+    Private String marca;
+    Private Int aro;
+
+
+    public Pneu(String marca, String aro) {
+        this.marca = marca;
+        this.aro = aro;
+    }
+
+
+
+    
+}
+
