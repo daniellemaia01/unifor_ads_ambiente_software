9a869e46633f0a30b6efda34bb903215e818d23a
Márcia Gomes <mlo_19@hotmail.com>
2023-11-30 14:15:17 -0300

    Método no CarroDanielle

--- atividade_final/src/main/java/br/unifor/ads/software/atividade_final/CarroDanielle.java
+++ atividade_final/src/main/java/br/unifor/ads/software/atividade_final/CarroDanielle.java
@@ -3,120 +3,97 @@
 package br.unifor.ads.software.atividade_final;
 
 public class CarroDanielle {
-    
-    //Atributos
-    
-    private String nome;
-    private String marca;
-    private String cor;
-    private boolean ligar;
-    
-    
-   // Métodos Especiais
-    
-    public CarroDanielle(String nome, String marca,String cor, boolean ligar) {
-    	
-    	this.nome = nome;
-    	this.marca = marca;
-    	this.cor = cor;
-    	this.ligar = ligar;
-    
-    }
-    
-    
-    public String getNome() {
-		return nome;
+
+	// Atributos
+
+	private String nome;
+	private String marca;
+	private String cor;
+	private boolean ligar;
+
+	// Métodos Especiais
+
+	public CarroDanielle(String nome, String marca, String cor, boolean ligar) {
+
+		this.nome = nome;
+		this.marca = marca;
+		this.cor = cor;
+		this.ligar = ligar;
+
 	}
 
-
-
-
+	public String getNome() {
+		return nome;
+	}
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-
-
-
-
 	public String getMarca() {
 		return marca;
 	}
-
-
-
-
 
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
 
-
-
-
-
 	public String getCor() {
 		return cor;
 	}
-
-
-
-
 
 	public void setCor(String cor) {
 		this.cor = cor;
 	}
 
-
-
-
-
 	public boolean isLigar() {
 		return ligar;
 	}
-
-
-
-
 
 	public void setLigar(boolean ligar) {
 		this.ligar = ligar;
 	}
 
-
-  //Métodos
-
+	// Métodos
 
 	public void status() {
-    	System.out.println(this.nome);
-    	System.out.println(this.marca);
-    	System.out.println(this.cor);
-    	System.out.println(this.ligar);
-    	
-    }
-    public void ligarCarro() {
-    	this.ligar = true;
-    }
-    	
- // Método principal 
-    
-    public static void main(String[] args) {
-        
-    	//Testando meus métodos
-    	CarroDanielle d1 = new CarroDanielle("Tracker", "Chevrolet", "Branco", false);
-    	d1.ligarCarro();
-    	d1.status();
-    	
-    	//Chamando métodos da Classe MotorMarcia
-    	
-    	MotorMarcia m1 = new MotorMarcia("Flex", 116);
-    	System.out.println (m1.getPotencia());
-    	
-    	// Chamando método criado por Danielle na classe MotorMarcia
-        
-        m1.statusMotor();
-    }
+		System.out.println(this.nome);
+		System.out.println(this.marca);
+		System.out.println(this.cor);
+		System.out.println(this.ligar);
+
+	}
+
+	public void ligarCarro() {
+		this.ligar = true;
+	}
+
+	// Método principal
+
+	public static void main(String[] args) {
+
+		// Testando meus métodos
+		CarroDanielle d1 = new CarroDanielle("Tracker", "Chevrolet", "Branco", false);
+		d1.ligarCarro();
+		d1.status();
+
+		// Chamando métodos da Classe MotorMarcia
+
+		MotorMarcia m1 = new MotorMarcia("Flex", 116);
+		System.out.println(m1.getPotencia());
+
+		// Chamando método criado por Danielle na classe MotorMarcia
+
+		m1.statusMotor();
+	}
+
+	// Método Márcia
+
+	public void CarroDanielle() {
+
+		System.out.println("Marca: " + this.marca);
+		System.out.println("Cor: " + this.cor);
+		System.out.println("Nome " + this.nome);
+
+	}
 }
-     
-      
